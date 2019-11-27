0caa96567987df30ee6f8f636c4830f72383a072
zhangxv <895470730@qq.com>
2019-11-27 23:27:25 +0800

    首页初始化

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -2,20 +2,20 @@ import React from 'react';
 import styles from '../assets/style/HomePage.css';
 
 const HomePage = () => {
-
 	return(
 		<div className={styles.HomePage}>
 			<nav className={`${styles.header} navbar navbar-default`}>
+				<div className={styles.title}>HTML</div>
 				<div className="container-fluid">
 					<div className="navbar-header">
 						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
 										data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 							<span className="sr-only">Toggle navigation</span>
-							<span className="icon-bar"> </span>
-							<span className="icon-bar"> </span>
-							<span className="icon-bar"> </span>
+							<span className={`${styles.line} icon-bar`}> </span>
+							<span className={`${styles.line} icon-bar`}> </span>
+							<span className={`${styles.line} icon-bar`}> </span>
 						</button>
-						<span className="navbar-brand">zhang xv</span>
+						<span className={`${styles.logo} navbar-brand`}>zhang xv</span>
 					</div>
 					<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul className="nav navbar-nav">
@@ -30,9 +30,9 @@ const HomePage = () => {
 					</div>
 				</div>
 			</nav>
-			<div className={styles.body}>
-
-			</div>
+			<mian className={styles.body}>
+				这里是文章列表
+			</mian>
 			<footer className={styles.footer}>
 				©zhangxv 2019-2020
 			</footer>
